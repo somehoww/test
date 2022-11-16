ae72f0ce414530e26e1ddd55eefce7a960fa452f
somehoww <103100724+somehoww@users.noreply.github.com>
2022-11-16 23:41:43 +0700

    Add files via upload

---
+++ enroll.php
@@ -0,0 +1,205 @@
+<?php
+session_start();
+include('includes/config.php');
+if(strlen($_SESSION['login'])==0 or strlen($_SESSION['pcode'])==0)
+    {   
+header('location:index.php');
+}
+else{
+
+if(isset($_POST['submit']))
+{
+$studentregno=$_POST['studentregno'];
+$pincode=$_POST['Pincode'];
+$session=$_POST['session'];
+$dept=$_POST['department'];
+$course=$_POST['course'];
+$sem=$_POST['sem'];
+$ret=mysqli_query($bd, "insert into courseenrolls(studentRegno,pincode,session,department,course,semester) values('$studentregno','$pincode','$session','$dept','$course','$sem')");
+if($ret)
+{
+$_SESSION['msg']="Đăng ký thành công !!";
+}
+else
+{
+  $_SESSION['msg']="Lỗi : không thể đăng ký !!";
+}
+}
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Đăng ký môn học</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />
+</head>
+
+<body>
+<?php include('includes/header.php');?>
+    <!-- LOGO HEADER END-->
+<?php if($_SESSION['login']!="")
+{
+ include('includes/menubar.php');
+}
+ ?>
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+              <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-head-line">Đăng ký môn học </h1>
+                    </div>
+                </div>
+                <div class="row" >
+                  <div class="col-md-3"></div>
+                    <div class="col-md-6">
+                        <div class="panel panel-default">
+                        <div class="panel-heading">
+                         Đăng ký môn học
+                        </div>
+<font color="green" align="center"><?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?></font>
+<?php $sql=mysqli_query($bd, "select * from students where StudentRegno='".$_SESSION['login']."'");
+$cnt=1;
+while($row=mysqli_fetch_array($sql))
+{ ?>
+
+                        <div class="panel-body">
+                       <form name="dept" method="post" enctype="multipart/form-data">
+   <div class="form-group">
+    <label for="studentname">Tên sinh viên  </label>
+    <input type="text" class="form-control" id="studentname" name="studentname" value="<?php echo htmlentities($row['studentName']);?>"  />
+  </div>
+
+ <div class="form-group">
+    <label for="studentregno">Mã số sinh viên  </label>
+    <input type="text" class="form-control" id="studentregno" name="studentregno" value="<?php echo htmlentities($row['StudentRegno']);?>"  placeholder="Student Reg no" readonly />
+    
+  </div>
+
+
+
+<div class="form-group">
+    <label for="Pincode">Mã xác thực</label>
+    <input type="text" class="form-control" id="Pincode" name="Pincode" readonly value="<?php echo htmlentities($row['pincode']);?>" required />
+  </div>   
+
+<div class="form-group">
+    <label for="Pincode">Ảnh hồ sơ  </label>
+   <?php if($row['studentPhoto']==""){ ?>
+   <img src="studentphoto/noimage.png" width="200" height="200"><?php } else {?>
+   <img src="studentphoto/<?php echo htmlentities($row['studentPhoto']);?>" width="200" height="200">
+   <?php } ?>
+  </div>
+ <?php } ?>
+
+<div class="form-group">
+    <label for="Session">Buổi học </label>
+    <select class="form-control" name="session" required="required">
+   <option value="">Chọn buổi học</option>   
+   <?php 
+$sql=mysqli_query($bd, "select * from session");
+while($row=mysqli_fetch_array($sql))
+{
+?>
+<option value="<?php echo htmlentities($row['id']);?>"><?php echo htmlentities($row['session']);?></option>
+<?php } ?>
+
+    </select> 
+  </div> 
+
+
+
+<div class="form-group">
+    <label for="Department">Phòng học </label>
+    <select class="form-control" name="department" required="required">
+   <option value="">Chọn phòng học</option>   
+   <?php 
+$sql=mysqli_query($bd, "select * from department");
+while($row=mysqli_fetch_array($sql))
+{
+?>
+<option value="<?php echo htmlentities($row['id']);?>"><?php echo htmlentities($row['department']);?></option>
+<?php } ?>
+
+    </select> 
+  </div> 
+
+
+<div class="form-group">
+    <label for="Semester">Kỳ học</label>
+    <select class="form-control" name="sem" required="required">
+   <option value="">Chọn kỳ học</option>   
+   <?php 
+$sql=mysqli_query($bd, "select * from semester");
+while($row=mysqli_fetch_array($sql))
+{
+?>
+<option value="<?php echo htmlentities($row['id']);?>"><?php echo htmlentities($row['semester']);?></option>
+<?php } ?>
+
+    </select> 
+  </div>
+
+
+<div class="form-group">
+    <label for="Course">Môn học </label>
+    <select class="form-control" name="course" id="course" onBlur="courseAvailability()" required="required">
+   <option value="">Chọn môn học</option>   
+   <?php 
+$sql=mysqli_query($bd ,"select * from course");
+while($row=mysqli_fetch_array($sql))
+{
+?>
+<option value="<?php echo htmlentities($row['id']);?>"><?php echo htmlentities($row['courseName']);?></option>
+<?php } ?>
+    </select> 
+    <span id="course-availability-status1" style="font-size:12px;">
+  </div>
+
+
+
+ <button type="submit" name="submit" id="submit" class="btn btn-default">Đăng ký</button>
+</form>
+                            </div>
+                            </div>
+                    </div>
+                  
+                </div>
+
+            </div>
+
+
+
+
+
+        </div>
+    </div>
+  <?php include('includes/footer.php');?>
+    <script src="assets/js/jquery-1.11.1.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+<script>
+function courseAvailability() {
+$("#loaderIcon").show();
+jQuery.ajax({
+url: "check_availability.php",
+data:'cid='+$("#course").val(),
+type: "POST",
+success:function(data){
+$("#course-availability-status1").html(data);
+$("#loaderIcon").hide();
+},
+error:function (){}
+});
+}
+</script>
+
+
+</body>
+</html>
+<?php } ?>
